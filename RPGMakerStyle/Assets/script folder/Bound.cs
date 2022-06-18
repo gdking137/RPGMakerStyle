b5529e87e0adf5ccfa1fc79dc02131de21a93cbd
gdking137 <gdkim137@gmail.com>
2022-06-17 21:37:49 -0400

    End of camera setting
    Has set the setting for the boundaries of the map. Now the camera wont go outside of the boundaries even if my player teleport to another place in the scene or even to another scene

---
+++ RPGMakerStyle/Assets/script folder/Bound.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bound : MonoBehaviour
+{
+    private BoxCollider2D bound;
+
+    private cameraManager theCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bound = GetComponent<BoxCollider2D>();
+        theCamera = FindObjectOfType<cameraManager>();
+        theCamera.SetBound(bound);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- RPGMakerStyle/Assets/script folder/TransferMap.cs
+++ RPGMakerStyle/Assets/script folder/TransferMap.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+
 
 public class TransferMap : MonoBehaviour
 {
@@ -9,16 +9,15 @@ public class TransferMap : MonoBehaviour
     private MovingObject thePlayer;
     private cameraManager theCamera;
     public string transferMapName;
-    public bool flag;
+
+    public BoxCollider2D targetBound;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        if (!flag)
-        {
-            theCamera = FindObjectOfType<cameraManager>();
-        }
+
+        theCamera = FindObjectOfType<cameraManager>();
         thePlayer = FindObjectOfType<MovingObject>();
        
     }
@@ -27,14 +26,8 @@ public class TransferMap : MonoBehaviour
     {
         if (collision.gameObject.gameObject.name == "Player")
         {
-            if(flag)
-                SceneManager.LoadScene(transferMapName);
-            else
-            //SceneManager.LoadScene(transferMapName);
             thePlayer.currentMapName = transferMapName;
-
-            thePlayer.transform.position = target.transform.position;
-
+            theCamera.SetBound(targetBound);
 
             thePlayer.transform.position = target.transform.position;
             theCamera.transform.position = new Vector3(target.transform.position.x, target.transform.position.y, theCamera.transform.position.z);

028f4956c16a565812d7c654259e4aa009ea9aa0
gdking137 <gdkim137@gmail.com>
2022-06-21 16:26:57 -0400

    NPC moving finished
    Moving editting finished. Now can move around with certain traits without any control. Also the animation doesnt look as weirder than before

--- RPGMakerStyle/Assets/script folder/MovingObject.cs
+++ RPGMakerStyle/Assets/script folder/MovingObject.cs
@@ -10,11 +10,90 @@ public class MovingObject : MonoBehaviour
     protected int currentWalkCount;
 
 
- 
+    protected bool npcCanMove = true;
 
     public BoxCollider2D boxCollider;
     public LayerMask layerMask;
     public Animator animator;
 
-    protected  Vector3 vector;
+    protected Vector3 vector;
+
+    protected void Move(string _dir, int _frequency)
+    {
+        StartCoroutine(MoveCoroutine(_dir, _frequency));
+    }
+
+
+    IEnumerator MoveCoroutine(string _dir, int _frequency)
+    {
+        npcCanMove = false;
+        vector.Set(0, 0, vector.z);
+        switch(_dir)
+        {
+            case "Up":
+                vector.y = 1f;
+                break;
+            case "Down":
+                vector.y = -1f;
+
+                break;
+            case "Right":
+                vector.x = 1f;
+                break;
+            case "Left":
+                vector.x = -1f; 
+                break;
+        }
+
+
+        animator.SetFloat("DirX", vector.x);
+        animator.SetFloat("DirY", vector.y);
+        animator.SetBool("Walking", true);
+
+
+
+        while (currentWalkCount < walkCount)
+        {
+
+            transform.Translate(vector.x * speed, vector.y * speed, 0);
+            currentWalkCount++;
+            yield return new WaitForSeconds(0.01f);
+
+
+
+
+        }
+
+        currentWalkCount = 0;
+        if(_frequency != 5)
+        animator.SetBool("Walking", false);
+
+        npcCanMove=true;
+
+    }
+
+    protected bool CheckCollsion()
+    {
+        RaycastHit2D hit;
+        //shoot raser and see if something hits or not
+        //position A + B
+        // hit = null
+        // hit = object
+
+        Vector2 start = transform.position;// A point player's current position
+        Vector2 end = start + new Vector2(vector.x * speed * walkCount, vector.y * speed * walkCount); // B Point player's next position
+
+
+        boxCollider.enabled = false;
+
+        hit = Physics2D.Linecast(start, end, layerMask);
+
+        boxCollider.enabled = true;
+
+        if (hit.transform != null) //if there is a wall what comes after this won't be affective
+            return true;
+        return false;
+
+    }
+
 }
--- RPGMakerStyle/Assets/script folder/NPCManager.cs
+++ RPGMakerStyle/Assets/script folder/NPCManager.cs
@@ -12,12 +12,12 @@ public class NPCMove
 
 
     [Range(1, 5)]
-    public int frequency; // npc rate of towards the chosen direction
+    public int frequency; // npc speed/rate/tempo of towards the chosen direction
 
 }
 
 
-public class NPCManager : MonoBehaviour
+public class NPCManager : MovingObject
 {
     [SerializeField]
 
@@ -25,12 +25,58 @@ public class NPCManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(MoveCoroutine());
     }
 
     // Update is called once per frame
-    void Update()
+public void SetMove()
     {
-        
+
     }
+
+
+public void SetNotMove()
+    {
+
+    }
+
+
+    IEnumerator MoveCoroutine()
+    {
+        if (npc.direction.Length != 0)
+        {
+            for (int i = 0; i < npc.direction.Length; i++)
+            {
+                switch (npc.frequency)
+                {
+                    case 1:
+                        yield return new WaitForSeconds(4f);
+                        break;
+                    case 2:
+                        yield return new WaitForSeconds(3f);
+                        break;
+                    case 3:
+                        yield return new WaitForSeconds(2f);
+                        break;
+                    case 4:
+                        yield return new WaitForSeconds(1f);
+                        break;
+                    case 5:
+                        break;
+
+                }
+                yield return new WaitUntil(() => npcCanMove);     //wait until npcCanMove
+
+                base.Move(npc.direction[i], npc.frequency);
+                //moving space
+
+                if (i == npc.direction.Length - 1)
+                {
+                    i = -1;
+                }
+            }
+        }
+    }
+
+
 }

b04a45bee180e022f0f1400b5145cb5132bda79b
gdking137 <gdkim137@gmail.com>
2022-06-20 23:38:11 -0400

    adding another npc moving animation

--- RPGMakerStyle/Assets/script folder/NPCManager.cs
+++ RPGMakerStyle/Assets/script folder/NPCManager.cs
@@ -2,8 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class NPCMove
+{
+    [Tooltip("if you check NPCMove then NPC will move")] //adds description on the inspector
+    public bool NPCmove;
+
+    public string[] direction; //npc moving directions
+
+
+    [Range(1, 5)]
+    public int frequency; // npc rate of towards the chosen direction
+
+}
+
+
 public class NPCManager : MonoBehaviour
 {
+    [SerializeField]
+
+    public NPCMove npc;
     // Start is called before the first frame update
     void Start()
     {
